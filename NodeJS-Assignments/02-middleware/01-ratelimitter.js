b538f2c9f7f5d4f550bf98e79d7fd96183f8462b
Saquib Anjum <saquibanjum8969@gmail.com>
2026-04-04 17:46:44 +0530

    NodeJS Assignment | 02 middleware

--- NodeJS-Assignments/02-middleware/01-ratelimitter.js
+++ NodeJS-Assignments/02-middleware/01-ratelimitter.js
@@ -1,6 +1,6 @@
 // You have to create a middleware for rate limiting a users request based on their username passed in the header
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Your task is to create a global middleware (app.use) which will
@@ -13,15 +13,39 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
-
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+  numberOfRequestsForUser = {};
+}, 1000);
+//globle rate limiter middleware
+app.use((req, res, next) => {
+  const userId = req.headers["user-id"];
+  if (!userId) {
+    return res.status(404).send("User Not Found");
+  }
+  // First request from this user
+  if (!numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] = 1;
+    // it will increase the request of user by 1
+  } else {
+    numberOfRequestsForUser[userId]++;
+  }
+  if (numberOfRequestsForUser[userId] > 5) {
+    return res.status(404).send("request out of limit");
+  }
+  next();
+});
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
-
+const PORT = 3000;
+if (require.main == module) {
+  app.listen(PORT, () => {
+    console.log(
+      `server is running on port : ${PORT} || http://localhost:${PORT}`,
+    );
+  });
+}
 module.exports = app;
--- NodeJS-Assignments/02-middleware/01-requestcount.js
+++ NodeJS-Assignments/02-middleware/01-requestcount.js
@@ -1,6 +1,6 @@
 // You have to create a middleware for logging the number of requests on a server
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 let requestCount = 0;
@@ -10,16 +10,29 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+// run this text command to [ npm run test-requestcount ]
+//globel middleware
+app.use((req, res, next) => {
+  requestCount++;
+  next();
+});
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
-app.get('/requestCount', function(req, res) {
+app.get("/requestCount", function (req, res) {
   res.status(200).json({ requestCount });
 });
-
+const PORT = 3000;
+if (require.main == module) {
+  app.listen(PORT, () => {
+    console.log(
+      `server is running on port :$P{PORT} || http://localhost:${PORT}`,
+    );
+  });
+}
 module.exports = app;
